a38ac5b2dda9fd03e5e36b4e6141e59ef5472c8b
zicheng-pan <zpan2@jaguarlandrover.com>
2023-06-12 17:40:56 +0800

    添加嵌入式tomcat和启动函数Main，可以直接在main方法中启动tomcat，但是访问的时候不需要加入build的项目名，直接访问servlet路径即可，例如http://localhost:8080/helloworld

---
+++ src/main/java/com/minispring/Main.java
@@ -0,0 +1,29 @@
+package com.minispring;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) throws LifecycleException {
+        // 启动 tomcat
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(Integer.getInteger("port", 8080));
+        tomcat.getConnector();
+        // 创建 WebApp
+        Context context = tomcat.addWebapp("", new File("src/main/webapp").getAbsolutePath());
+        WebResourceRoot resources = new StandardRoot(context);
+        resources.addPreResources(
+                new DirResourceSet(resources, "/WEB-INF/classes",
+                        new File("target/classes").getAbsolutePath(), "/"));
+        context.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
